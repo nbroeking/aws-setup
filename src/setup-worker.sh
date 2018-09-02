5c4914ceba179e0754b2ea392a21e479cbdde95c
Mr. Nic <nbroeking@users.noreply.github.com>
2018-09-02 10:17:39 -0600

    Use Docker-ce instead of docker.io

--- src/setup-worker.sh
+++ src/setup-worker.sh
@@ -10,7 +10,23 @@ ufw enable
 apt update
 
 #Apt
-apt -y install docker.io
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
+sudo apt-get install docker-ce
+
+sudo apt-get update
 
 #Install Kubernetes
 apt-get update && apt-get install -y apt-transport-https
