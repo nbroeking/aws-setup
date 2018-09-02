6fcbec573b47be6b5a4bedbad0eae999fe97787c
Mr. Nic <nbroeking@users.noreply.github.com>
2018-09-02 10:17:15 -0600

    Use docker-ce instead of docker.io

--- src/setup-master.sh
+++ src/setup-master.sh
@@ -8,11 +8,29 @@ ufw allow 6443
 ufw enable
 
 #Upgrade System
-apt update
+apt update && apt -y upgrade
 
-#Apt
-apt -y install docker.io
+# Docker
 
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
+sudo apt-get install docker-ce
+
+sudo apt-get update
+
+#Kill Swap
 swapoff -a
 
 #Install Kubernetes
